2ebc5c1171d0c254fcd56433bd36dc0ba12e32eb
ignacioportigliatti <ign.portigliatti@gmail.com>
2023-05-16 17:58:34 -0300

    Repo Started

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ['class'],
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,6 +8,11 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'dark-gray': '#121212',
+        'medium-gray': '#191919',
+        'light-gray': '#999999',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
